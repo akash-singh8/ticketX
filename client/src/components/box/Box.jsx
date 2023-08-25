64b3e2603a75a5993fc58e95e7ad6ea19e7a88ee
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-08-25 13:07:02 +0530

    adding custom style to box component, modifying navigation link in navbar, adding gethelp page

--- client/src/components/box/Box.jsx
+++ client/src/components/box/Box.jsx
@@ -4,11 +4,12 @@ import './box.css';
 
 export default function Box(props) {
   const ticketName = props.ticketName;
+  const specialStyle = props.specialStyle || false; 
   const linkPath = `/${ticketName.toLowerCase().replace(' ', '-')}`; 
 
   return (
     <Link to={linkPath} className="box-link"> 
-      <div className='box-container'>
+      <div className={`box-container ${specialStyle ? 'helpbox' : ''}`}>
         <div className='ticket-image'></div>
         <div className='ticket-name'>{ticketName}</div>
       </div>
--- client/src/pages/gethelp/Gethelp.jsx
+++ client/src/pages/gethelp/Gethelp.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import NavBar from '../../components/navbar/NavBar'
+import Box from '../../components/box/Box'
 
 export default function Gethelp() {
   return (
     <>
       <NavBar/>
+      <section  className='gethelp-tickets raiseTicket'>
+        <div className='heading'>
+            <div>SELECT CATEGORY <br></br>
+                 TO <span>GET HELP </span>WITH</div>
+        </div>
+        <div className='ticket-box '>
+            <Box ticketName='GET HELP'/>
+            <Box  ticketName='BUSINESS UPDATES'/>
+            <Box  ticketName='RESOURCES'/>
+            <Box  ticketName='RESOURCES'/>
+            <Box  ticketName='RESOURCES'/>
+            <Box  ticketName='RESOURCES'/>
+        </div>
+    </section>
     </>
   )
 }
