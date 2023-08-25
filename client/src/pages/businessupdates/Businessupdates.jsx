1c5b6d85e7fcabbda51505421a7dd7938bc5816e
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-08-25 19:27:29 +0530

    adding businessupdates page

--- client/src/pages/businessupdates/Businessupdates.jsx
+++ client/src/pages/businessupdates/Businessupdates.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Box from '../../components/box/Box'
+import Footer from '../../components/footer/Footer'
+import Navbar2 from '../../components/navbar2/Navbar2'
 
 export default function Businessupdates() {
   return (
-    <div>Businessupdates</div>
+    <>
+      <Navbar2/>
+      <section  className='gethelp-tickets raiseTicket'>
+        <div className='heading'>
+            <div>SELECT CATEGORY <br></br>
+                 TO <span>GET HELP </span>WITH</div>
+        </div>
+        <div className='ticket-box '>
+            <Box ticketName='NEW PRODUCT LAUNCH'/>
+            <Box  ticketName='MARKETING'/>
+            <Box  ticketName='EXPANSION OF BUSINESS'/>
+            <Box  ticketName='REVENUE'/>
+           
+            <Box  ticketName='OTHERS'/>
+        </div>
+    </section>
+    <Footer/>
+    </>
   )
 }
