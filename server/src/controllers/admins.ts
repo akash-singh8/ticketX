8bd74a883ee91f2ba9996c0674285410cf10d18d
Akash Singh <developer.akash8@gmail.com>
2023-09-14 00:49:54 +0600

    controller: handle admin login

--- server/src/controllers/admins.ts
+++ server/src/controllers/admins.ts
@@ -3,8 +3,11 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import sendMail from "./sendMail.js";
 import Admins from "../models/Admins.js";
-import { AdminSignupData } from "../types/admin.js";
-import { adminSignupInputSchema } from "../validation/admin.js";
+import { AdminLoginData, AdminSignupData } from "../types/admin.js";
+import {
+  adminLoginInputSchema,
+  adminSignupInputSchema,
+} from "../validation/admin.js";
 
 export const handleAdminSignup = async (req: Request, res: Response) => {
   const bodyData: AdminSignupData = req.body;
@@ -58,6 +61,60 @@ export const handleAdminSignup = async (req: Request, res: Response) => {
     res
       .status(500)
       .json({ message: "Internal server error during admin signup" });
+    console.log(err);
+  }
+};
+
+export const handleAdminLogin = async (req: Request, res: Response) => {
+  const bodyData: AdminLoginData = req.body;
+
+  const isValidInput = adminLoginInputSchema.safeParse(bodyData);
+
+  if (!isValidInput.success) {
+    res.status(400).json({
+      message: isValidInput.error.issues[0].message,
+      error: isValidInput.error,
+    });
+    return;
+  }
+
+  const { email, password } = isValidInput.data;
+
+  try {
+    const admin = await Admins.findOne({ email });
+
+    if (!admin) {
+      res
+        .status(404)
+        .json({ message: `The admin with the email ${email} does not exist.` });
+      return;
+    }
+
+    if (admin.banned) {
+      res.status(403).json({ message: `Admin ${email} is banned.` });
+      return;
+    }
+
+    const isValidPswd = await bcrypt.compare(password, admin.password);
+
+    if (!isValidPswd) {
+      res.status(401).json({ message: "Invalid password" });
+      return;
+    }
+
+    if (!process.env.JWT_AUTH_SECRET) {
+      throw new Error("JWT_AUTH_SECRET environment variable is not defined.");
+    }
+
+    const token = jwt.sign({ id: admin._id }, process.env.JWT_AUTH_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ message: "Logged successfully", authToken: token });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Internal server error during admin login" });
     console.log(err);
   }
 };
