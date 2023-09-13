09d59f4f888a18e454ad8405d2880601b5000ebd
Akash Singh <developer.akash8@gmail.com>
2023-09-14 00:32:57 +0600

    admin model: added fields for validation

--- server/src/models/Admins.ts
+++ server/src/models/Admins.ts
@@ -20,6 +20,26 @@ const AdminSchema = new mongoose.Schema({
       ref: "tickets",
     },
   ],
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  OTP: {
+    type: String,
+    required: true,
+  },
+  OTP_Attempt: {
+    type: Number,
+    default: 0,
+  },
+  incorrectAttempt: {
+    type: Number,
+    default: 0,
+  },
+  banned: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Admins = mongoose.model("Admin", AdminSchema);
