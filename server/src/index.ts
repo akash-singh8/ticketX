2ae1eef9fe8b45f77bc287c3157b9419b5642b3b
Akash Singh <developer.akash8@gmail.com>
2023-09-13 17:52:17 +0600

    index: converted to ts

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const mongodb_uri = process.env.MONGODB_URI;
+
+if (!mongodb_uri) {
+  console.error("MONGODB_URI environment variable is not defined.");
+} else {
+  mongoose.connect(mongodb_uri, { dbName: "ticketPortal" });
+
+  const db = mongoose.connection;
+
+  db.once("open", () => {
+    console.log("MongoDB connected");
+
+    const PORT: number = parseInt(process.env.PORT || "3000", 10);
+
+    app.listen(PORT, () => {
+      console.log("Server listening on port:", PORT);
+    });
+  });
+
+  db.on("error", (error) => {
+    console.error("MongoDB connection error:", error);
+  });
+}
