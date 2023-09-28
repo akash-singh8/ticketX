5a3d89b47abc0e063343b2dde426f1fe5f8bbfd8
Akash Singh <developer.akash8@gmail.com>
2023-09-28 16:45:35 +0600

    validation: added for input ticket data

---
+++ server/src/validation/ticket.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+const userRaisedSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Name must be at least 3 characters")
+    .max(100, "Name cannot exceed 100 characters"),
+  email: z
+    .string()
+    .min(5, "Email must be at least 5 characters")
+    .max(200, "Email cannot exceed 200 characters")
+    .email("Email must be a valid email address"),
+});
+
+const ticketSchema = z.object({
+  category: z
+    .string()
+    .min(3, "Category must be at least 3 characters")
+    .max(25, "Category cannot exceed 25 characters"),
+  title: z
+    .string()
+    .min(3, "Title must be at least 3 characters")
+    .max(200, "Title cannot exceed 200 characters"),
+  message: z
+    .string()
+    .min(3, "Message must be at least 3 characters")
+    .max(1000, "Message cannot exceed 1000 characters"),
+  dateRaised: z.date(),
+  status: z.enum(["pending", "inreview", "resolved"]),
+  raisedBy: userRaisedSchema,
+});
+
+export default ticketSchema;
