a1daae4f3b0b3eee7a65e267cc7b73a7ac78b105
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-10-01 01:34:31 +0530

    reflecting login changes in hero section

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useModal } from "../modalProvider/Modalprovider";
 
 export default function Hero() {
-  const {openSignupModal} = useModal();
+  const {openSignupModal,isAuthenticated,user} = useModal();
   const navigate=useNavigate()
   const authData = localStorage.getItem("user");
   const auth = JSON.parse(authData);
@@ -33,11 +33,11 @@ export default function Hero() {
           Journey
         </div>
         <SignUp/>
-        {!auth && <div className="hero-button" text="REGISTER NOW" onClick={openSignupModal} >Register Now</div>}
-        {auth && auth.email==="client" && <>
+        {!isAuthenticated && <div className="hero-button" text="REGISTER NOW" onClick={openSignupModal} >Register Now</div>}
+        {isAuthenticated && user.role==="client" && <>
         <div className="hero-button" onClick={handleClickServices}>RAISE TICKETS</div>
         </>}
-        {auth && auth.email==="admin" && <>
+        {isAuthenticated && user.role==="admin" && <>
         <div className="hero-button" onClick={viewRequests}>VIEW TICKETS</div>
         </>}
       </div>
