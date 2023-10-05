7a2e59850d13714ecc7ced65cd1a727d0b95a39f
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-10-06 00:31:31 +0530

    forgot Pass/combining otp verification and reset password to single step

--- client/src/components/forgotPass/ForgotPass.jsx
+++ client/src/components/forgotPass/ForgotPass.jsx
@@ -4,6 +4,8 @@ import "./forgotPass.css"
 export default function ForgotPass() {
   const [email, setEmail] = useState('');
   const [otp, setOTP] = useState('');
+  const [pass, setpass] = useState('');
+  const [confirmpass, setconfirmpass] = useState('');
   const [step, setStep] = useState(1);
 
   const handleEmailSubmit = async (e) => {
@@ -18,7 +20,7 @@ export default function ForgotPass() {
 
   return (
     <div className='form-box'>{step === 1 && (
-      <form onSubmit={handleEmailSubmit}>
+      <form onSubmit={handleEmailSubmit} className='form'>
         <h2 className='heading forgot'>Forgot Password</h2>
         <label>Email:</label>
         <input className='input-box'
@@ -27,12 +29,15 @@ export default function ForgotPass() {
           onChange={(e) => setEmail(e.target.value)}
           required
         />
+        <div className='center'>
+
         <button className='button login-button' type="submit">Send OTP</button>
+        </div>
       </form>
     )}
     {step === 2 && (
-      <form onSubmit={handleOTPSubmit}>
-        <h2 className='heading forgot'>Verify OTP</h2>
+      <form onSubmit={handleOTPSubmit} className='form'>
+        <h2 className='heading'>Verify OTP</h2>
         <label>Enter OTP:</label>
         <input
         className='input-box'
@@ -41,14 +46,28 @@ export default function ForgotPass() {
           onChange={(e) => setOTP(e.target.value)}
           required
         />
-        <button  className='button login-button'type="submit">Verify OTP</button>
+        <label>New Password</label>
+        <input
+        className='input-box'
+          type="text"
+          value={pass}
+          onChange={(e) => setpass(e.target.value)}
+          required
+        />
+        <label> Confirm New Password</label>
+        <input
+        className='input-box'
+          type="text"
+          value={confirmpass}
+          onChange={(e) => setconfirmpass(e.target.value)}
+          required
+        />
+        <div className='center'>
+
+        <button  className='button login-button shift' type="submit">Verify OTP</button>
+        </div>
       </form>
     )}
-    {step === 3 && (
-      <div>
-        <h2>Password Reset</h2>
-        {/* Render your password reset form here */}
-      </div>
-    )}</div>
+  </div>
   )
 }
