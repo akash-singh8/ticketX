ce4a77fb470c826c9cb3c37aa206b269bb9cb081
Akash Singh <developer.akash8@gmail.com>
2023-10-06 01:26:55 +0600

    controller: set new password

--- server/src/controllers/forgot.ts
+++ server/src/controllers/forgot.ts
@@ -4,6 +4,8 @@ import Users from "../models/Users";
 import sendMail from "./sendMail";
 import validateEmail from "../validation/email";
 import Admins from "../models/Admins";
+import { otpInputSchema } from "../validation/otpValidation";
+import bcrypt, { hash } from "bcrypt";
 
 export const handleForgotPassword = async (req: Request, res: Response) => {
   const isValidEmail = validateEmail.safeParse(req.body);
@@ -49,6 +51,79 @@ export const handleForgotPassword = async (req: Request, res: Response) => {
     res
       .status(200)
       .json({ message: "successfully send new OTP", authToken: token });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Internal server error on forgot password" });
+    console.log(err);
+  }
+};
+
+export const setNewPassword = async (req: Request, res: Response) => {
+  const isValidOTP = otpInputSchema.safeParse(req.body);
+
+  if (!isValidOTP.success) {
+    return res.status(400).json({
+      message: isValidOTP.error.issues[0].message,
+      error: isValidOTP.error,
+    });
+  }
+
+  const newPassword = req.body.newPassword;
+
+  if (
+    typeof newPassword !== "string" ||
+    newPassword.length < 8 ||
+    newPassword.length > 200
+  ) {
+    return res.status(400).json({ message: "Invalid password" });
+  }
+
+  const userData: {
+    id: string;
+    role: string;
+  } = req.body.user;
+
+  const userEnteredOTP = parseInt(isValidOTP.data.OTP);
+
+  try {
+    let user;
+
+    if (userData.role === "admin") {
+      user = await Admins.findOne({ _id: userData.id });
+    } else {
+      user = await Users.findOne({ _id: userData.id });
+    }
+
+    if (!user) {
+      return res.status(403).json({ message: "User not found!" });
+    }
+
+    let OTP_decode;
+
+    try {
+      OTP_decode = jwt.verify(user.OTP, process.env.JWT_OTP_SECRET!);
+      if (typeof OTP_decode === "string") throw new Error("Invalid OTP Type");
+    } catch (err) {
+      res.status(404).json({ message: "OTP expired, resend new OTP!" });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 11);
+
+    if (userEnteredOTP === OTP_decode.OTP) {
+      await (userData.role === "admin" ? Admins : Users).updateOne(
+        { _id: user.id },
+        { password: hashedPassword }
+      );
+      res.status(200).json({ message: "Successfully updated password" });
+    } else {
+      await (userData.role === "admin" ? Admins : Users).updateOne(
+        { _id: user.id },
+        { $inc: { incorrectAttempt: 1 } }
+      );
+      res.status(404).json({ message: "Incorrect OTP" });
+    }
   } catch (err) {
     res
       .status(500)
