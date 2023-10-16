f74e0cc466fd1b306dfa9ab1a134f40fdf46d27a
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-10-16 15:08:58 +0530

    adding: integrating recent requests

--- client/src/components/recentrequests/Recentrequests.jsx
+++ client/src/components/recentrequests/Recentrequests.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Location from '../location/Location'
 import Reqbox from '../reqbox/Reqbox'
 import Pagenavigation from '../pagenavigation/Pagenavigation'
 export default function Recentrequests() {
+  const [getTickets, setGetTickets] = useState([]);
+
+  const fetchTickets = async (status) => {
+    try {
+      const authToken=localStorage.getItem("authorization")
+      const response = await fetch(
+        `http://localhost:3080/ticket/recent`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: authToken,
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        setGetTickets(data.tickets);
+      } else {
+        const errorData = await response.json();
+        throw new Error(`Failed to fetch tickets: ${errorData.message}`);
+      }
+    } catch (err) {
+      console.error("Error fetching tickets:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTickets();
+  }, []);
+
   
   return (
     <>
@@ -18,11 +49,17 @@ export default function Recentrequests() {
           
         <Location/>
         <div className='requests'>
+          {
 
-        <Reqbox />
-        <Reqbox />
-        <Reqbox />
-        <Reqbox />
+            getTickets.length > 0 ? (
+              getTickets.map((ticket) => (
+                <Reqbox key={ticket.id} ticket={ticket} />
+                ))
+                ) : (
+                  <h3 className="center">No Recent Tickets</h3>
+                  )
+                }
+
         </div>
         <Pagenavigation />
       </section>
