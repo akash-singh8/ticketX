fed53b81f37c38d6d348ca471e05b248761efefd
Akash Singh <developer.akash8@gmail.com>
2023-10-16 11:14:06 +0600

    route: get recently raised tickets

--- server/src/routes/ticket.ts
+++ server/src/routes/ticket.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { raiseTicket, updateTicketStatus } from "../controllers/tickets";
+import {
+  getRecentTickets,
+  raiseTicket,
+  updateTicketStatus,
+} from "../controllers/tickets";
 
 const ticketRouter = Router();
 
 // no need to validate stats each time, as it increase the request on database
 // for that add verified & banned status of the user in the token itself
+
+ticketRouter.get("/recent", getRecentTickets);
 
 ticketRouter.put("/update", updateTicketStatus);
 
