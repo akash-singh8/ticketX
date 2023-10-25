629083841e797de6cb316538fbd9690125878373
Akash Singh <developer.akash8@gmail.com>
2023-10-25 12:01:17 +0600

    admin details: populate ticket inreview

--- server/src/controllers/tickets.ts
+++ server/src/controllers/tickets.ts
@@ -13,7 +13,9 @@ export const getUser = async (req: Request, res: Response) => {
   try {
     let userData;
     if (user.role === "admin") {
-      userData = await Admins.findById(user.id).populate("ticketResolved");
+      userData = await Admins.findById(user.id)
+        .populate("ticketResolved")
+        .populate("ticketInReview");
     } else {
       userData = await Users.findById(user.id).populate("ticketRaised");
     }
