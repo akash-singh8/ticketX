c456dc451355280660b7b5c3297f23bc40fd5b62
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-11-03 19:26:04 +0530

    responsive : landing

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { useModal } from "../../modalProvider/Modalprovider";
 
 export default function Hero() {
-  const {openSignupModal,isAuthenticated,user} = useModal();
-  const navigate=useNavigate()
+  const { openSignupModal, isAuthenticated, user } = useModal();
+  const navigate = useNavigate();
   const handleClickServices = () => {
     navigate("/");
     setTimeout(() => {
@@ -17,9 +17,9 @@ export default function Hero() {
       }
     }, 100);
   };
-  const viewRequests=()=>{
-    navigate("/admin/requests")
-  }
+  const viewRequests = () => {
+    navigate("/admin/requests");
+  };
   return (
     <section id="hero">
       <div className="container1">
@@ -27,26 +27,42 @@ export default function Hero() {
           TICKET <br></br>PORTAL
         </div>
         <div className="center_class">
+          <div className="hero-details">
+            Unleash the Power of Collaboration and Fuel Your Entrepreneurial
+            Journey
+          </div>
+        </div>
+        <SignUp />
 
-        <div className="hero-details">
-          Unleash the Power of Collaboration and Fuel Your Entrepreneurial
-          Journey
-        </div>
-        </div>
-        <SignUp/>
-        
-        {!isAuthenticated && 
-        <div className="center_class">
-
-        <div className="hero-button center" text="REGISTER NOW" onClick={openSignupModal} >Register Now</div>
-        </div>
-        }
-        {isAuthenticated && user.role==="client" && <>
-        <div className="hero-button" onClick={handleClickServices}>RAISE TICKETS</div>
-        </>}
-        {isAuthenticated && user.ticketResolved && <>
-        <div className="hero-button" onClick={viewRequests}>VIEW TICKETS</div>
-        </>}
+        {!isAuthenticated && (
+          <div className="center_class">
+            <div
+              className="hero-button center"
+              text="REGISTER NOW"
+              onClick={openSignupModal}
+            >
+              Register Now
+            </div>
+          </div>
+        )}
+        {isAuthenticated && user.role === "client" && (
+          <>
+            <div className="center_class">
+              <div className="hero-button" onClick={handleClickServices}>
+                RAISE TICKETS
+              </div>
+            </div>
+          </>
+        )}
+        {isAuthenticated && user.ticketResolved && (
+          <>
+            <div className="center_class">
+              <div className="hero-button" onClick={viewRequests}>
+                VIEW TICKETS
+              </div>
+            </div>
+          </>
+        )}
       </div>
       <div className="image-container">
         <img className="hero-image " src={hero} alt="hero"></img>
