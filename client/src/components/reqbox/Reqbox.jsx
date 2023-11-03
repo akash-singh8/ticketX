b5124351046d8567d0b405451216251d78be11e5
Vaishali054 <100770875+Vaishali054@users.noreply.github.com>
2023-11-03 22:25:54 +0530

    responsive : req box

--- client/src/components/reqbox/Reqbox.jsx
+++ client/src/components/reqbox/Reqbox.jsx
@@ -7,9 +7,12 @@ export default function Reqbox(props) {
     <div className="req-box">
       <div className="container1-req">
         <div className="ticket-title">{ticket.title}</div>
+        <div className="message">
+
         {ticket.message.split(" ").length > 30
           ? ticket.message.split(" ").slice(0, 30).join(" ") + "..."
           : ticket.message}
+          </div>
       </div>
       <ViewReq ticket={ticket}/>
     </div>
